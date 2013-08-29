8af778b90c10407b7ba2a3daab336c6d1d200c18
John W. Fail <fail@cenotaph.org>
2013-08-29 15:52:53 +0300

    secret key moved to applicaiton

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,7 +9,7 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Aim::Application.config.secret_key_base = ENV['SECRET_KEY']
+
 
 
 if Rails.env.production?
