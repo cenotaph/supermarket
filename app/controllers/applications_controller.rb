6c6f53fa43d08c3420264cdc359bc034ca18c220
John W. Fail <fail@cenotaph.org>
2013-09-10 19:21:32 +0300

    custom error pages and now people can edit applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -17,7 +17,7 @@ class ApplicationsController < ApplicationController
   def edit
     @application = Application.find(params[:id])
     @space = @application.space
-    if @application.space.users.include?(current_user) && @application.status != 'active'
+    if @application.space.users.include?(current_user) # && @application.status != 'active'
       render :template => 'applications/new'
     else
       flash[:error] = 'You cannot edit this application.'
