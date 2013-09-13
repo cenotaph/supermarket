63281208735e84b0b098fe1153f77f574311da78
John W. Fail <fail@cenotaph.org>
2013-09-13 16:10:41 +0300

    display website with/out http

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,10 @@
 class Website < ActiveRecord::Base
   belongs_to :application
+  
+  def url_safe
+    unless self.url[/^http:\/\//] || self.url[/^https:\/\//]
+      self.url = 'http://' + self.url
+    end
+  end
+  
 end
