a0218766fdb5516e6be1b1b45fbd8a596e1c60a2
John W. Fail <fail@cenotaph.org>
2013-09-22 12:23:48 +0300

    safe url

--- app/models/applicationlink.rb
+++ app/models/applicationlink.rb
@@ -2,7 +2,7 @@ class Applicationlink < ActiveRecord::Base
   belongs_to :application
   validates_presence_of :url, :application_id
   
-  def url_safe
+  def self.url_safe
     unless self.url[/^http:\/\//] || self.url[/^https:\/\//]
       self.url = 'http://' + self.url
     end
