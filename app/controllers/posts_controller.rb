22c27aeb3e88bc3bb1f4de15e444b37684909928
John W. Fail <fail@cenotaph.org>
2014-02-09 18:39:16 +0100

    new posts display and removing assets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   
+  def index
+    @posts = Post.by_subsite(@subsite.id).published.order('published_at DESC').page(params[:page]).per(5)
+  end
+  
   def show
     @post = Post.friendly.find(params[:id])
   end
