b4c49de39810dd1ad8a7a59ae0401b596254d724
John W. Fail <fail@cenotaph.org>
2014-09-17 20:32:21 +0300

    red supermarket 2015 theme

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,9 +11,9 @@ class SearchController < ApplicationController
     # @hits += Page.by_subsite(@subsite.id).includes(:translations).advanced_search(params[:searchterm])
     @hits += Post.by_subsite(@subsite.id).includes(:translations).advanced_search(params[:searchterm]).published
     
-    @hits += Application.fuzzy_search(params[:searchterm]).delete_if{|x| !x.approved?}.map(&:space)
-    @hits += Space.fuzzy_search(params[:searchterm]).delete_if {|x| x.applications.approved.empty? } 
-    @hits += Space.where(["lower(country) = ? OR lower(visiting_country) = ?", countrycode, countrycode]).delete_if {|x| x.applications.approved.empty? } unless countrycode.nil?
+    @hits += Application.fuzzy_search(params[:searchterm]).to_a.delete_if{|x| !x.approved?}.map(&:space)
+    @hits += Space.fuzzy_search(params[:searchterm]).to_a.delete_if {|x| x.applications.approved.empty? } 
+    @hits += Space.where(["lower(country) = ? OR lower(visiting_country) = ?", countrycode, countrycode]).to_a.delete_if {|x| x.applications.approved.empty? } unless countrycode.nil?
 
     @hits.uniq!
   end
