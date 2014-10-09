db09b68c304f50ad9fc8d22cd99bcde911a403eb
John W. Fail <fail@cenotaph.org>
2014-10-09 16:26:59 +0300

    fixed super-TV year mismatch

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,7 +7,7 @@ class VideosController < InheritedResources::Base
     else
       @year = Year.where(:reveal_decisions => true).order("year DESC").first
     end
-    @years = Year.all.order("year desc").delete_if{|x| x.videos.published.empty? }
+    @years = Year.all.order("year desc").to_a.delete_if{|x| x.videos.published.empty? }
     @videos = []
     #@years.each do |year|
     @videos = Video.published.order('created_at DESC').where("year_id  = ?", @year.id).page(params[:page]).per(6) 
