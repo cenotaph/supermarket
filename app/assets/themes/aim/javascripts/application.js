12c5bc38d230417db366e3caeb06a4d49a8d3a87
John W. Fail <fail@cenotaph.org>
2015-02-01 16:23:27 +0100

    new design with andreas

--- app/assets/themes/aim/javascripts/application.js
+++ app/assets/themes/aim/javascripts/application.js
@@ -1,4 +1,33 @@
 //= require aim/javascripts/jquery.jstree
+//= require jquery.easing
 //= require foundation
 //= require chosen-jquery
 
+function toggle_filters() {
+
+  if ($('#filters').is(":visible") ) {
+    $('#filters').animate({width: 'toggle'}); //stop(true,true).hide("slide", { direction: "left" }, 200);
+    $('.toggle_filters').animate({left:  "-=20%"});
+    $('.toggle_filters img').attr('src', '/assets/aim/images/magnifier.png');
+  } else {
+    $('#filters').animate({width: 'toggle'}); //("slide", { direction: "left" }, 200);
+    $('.toggle_filters').animate({left:  "+=20%"});
+    $('.toggle_filters img').attr('src', '/assets/aim/images/back_arrow.png');
+  }
+
+}
+
+function toggle_sign_in() {
+  var boxheight = $('.login_bottom').css('height');
+  alert(boxheight);
+  if ($('#user_info').is(":visible") ) {
+    $('.login_bottom ').slideToggle();
+    $('.sign_in').animate({top:  "-=2rem"});
+    //$('.sign_in img').attr('src', '/assets/aim/images/magnifier.png');
+  } else {
+    $('.login_bottom ').slideToggle();
+    $('.sign_in').animate({top:  "+=" + boxheight});
+    //$('.toggle_filters img').attr('src', '/assets/aim/images/back_arrow.png');
+  }
+
+}
