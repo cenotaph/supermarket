ed4733dde2a752328f3bc8625306087c45092422
John W. Fail <fail@cenotaph.org>
2015-03-06 21:27:36 +0200

    desperate now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Aim::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor =  Uglifier.new(compress: { unused: false}) if defined? Uglifier
+  config.assets.js_compressor =  :uglifier # Uglifier.new(compress: { unused: false}) if defined? Uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
