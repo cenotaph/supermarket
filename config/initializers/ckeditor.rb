3a56a247d35eff1efaa5cd7a951e436ca9233b34
John W. Fail <fail@cenotaph.org>
2015-03-07 09:22:08 +0200

    trying again

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,5 +1,5 @@
 Ckeditor.setup do |config|
   require "ckeditor/orm/active_record"
-  config.assets_languages = ['en']
+  # config.assets_languages = ['en']
   config.assets_plugins = ['image', 'filebrowser', 'link']
 end
