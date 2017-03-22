ad4e867c932085a1c2c8fe6a20a7365223c1780d
John W. Fail <john@pixelache.ac>
2017-03-22 15:56:28 +0100

    ckeditor 4.6.2

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,5 +1,5 @@
 Ckeditor.setup do |config|
   require "ckeditor/orm/active_record"
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.6.1/standard-all/ckeditor.js"
+  config.cdn_url = "//cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"
 end
