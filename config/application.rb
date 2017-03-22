3be497a4ca479d4f425aa080cf294d7bc9a05bf3
John W. Fail <john@pixelache.ac>
2017-03-22 15:49:16 +0100

    ckeditor from csn

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module Aim
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     
     config.secret_key_base = ENV['SECRETKEY']
