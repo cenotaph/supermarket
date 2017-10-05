89e5b2271d060a55edbd51db90f34f19199c0bc9
John W. Fail <john@pixelache.ac>
2017-10-05 10:00:14 +0300

    2017 to 2018

--- config/routes.rb
+++ config/routes.rb
@@ -47,11 +47,11 @@ Aim::Application.routes.draw do
 
   get '/user', to: 'users#profile'
   root 'application#frontpage'
-  get '/apply/2017/:space_id', to: 'applications#new', :year => 2017
-  get '/apply/2017', to: 'applications#applylanding'
+  get '/apply/2018/:space_id', to: 'applications#new', :year => 2018
+  get '/apply/2018', to: 'applications#applylanding'
   get '/invited', to: 'applications#invited'
   post '/invited', to: 'applications#check_invited'
-  get '/invited/2017/:space_id', to: 'applications#allow_late', :year => 2017
+  get '/invited/2018/:space_id', to: 'applications#allow_late', :year => 2018
   get '/admin', to: 'admin/dashboard#index', as: :admin_dashboard
   get '/exhibitors/:year', to: 'exhibitors#year'
   get '/pnp', to: 'pnps#index'
