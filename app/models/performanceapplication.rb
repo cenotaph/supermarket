1b75d013b6fa4c1c65095efd6173056682d4ef0d
John W. Fail <john@pixelache.ac>
2017-11-03 15:26:41 +0200

    do not save attachment metadata

--- app/models/performanceapplication.rb
+++ app/models/performanceapplication.rb
@@ -3,27 +3,27 @@ class Performanceapplication < ActiveRecord::Base
   belongs_to :year
   validates_presence_of :contact_name, :user_id, :year_id, :contact_email, :contact_phone, :description, :prep_time, :performance_time, :num_of_participants_on_stage, :number_of_participants_off_stage, :technical_requirements
   mount_uploader :attachment, AttachmentUploader
-  before_save :get_attachment_metadata
+  # before_save :get_attachment_metadata
   scope :by_year, ->(x) { where(:year_id => x)}
   before_save :shorten_long_strings
   has_many :performanceapplicationcomments, :dependent => :destroy, :foreign_key => :app_id
-  
+
   def shorten_long_strings
     self.description = self.description[0..999]
     self.connection_to_visual_arts = self.connection_to_visual_arts[0..499]
   end
-  
+
   def get_attachment_metadata
     if attachment.present?
       if attachment.file.exists?
         self.attachment = attachment.file.content_type
-        self.attachment_file_size = attachment.file.size
+        self.attachment_file_size = attachment.file.size rescue 0
       end
-    end    
+    end
   end
-  
+
   def decision_text
-    case decision 
+    case decision
     when nil
       'not decided yet'
     when 1
@@ -34,5 +34,5 @@ class Performanceapplication < ActiveRecord::Base
       'Maybe'
     end
   end
-  
+
 end
