0027ad142bee9925742b1c7bdc1ea30a7344ca77
John W. Fail <john@pixelache.ac>
2019-02-11 13:00:46 +0200

    remove bogus model

--- app/models/add_informalcountry_to_application.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddInformalCountryToApplications < ActiveRecord::Migration
-  def change
-    execute('alter table applications add column informal_country varchar(255)')
-  end
-end
