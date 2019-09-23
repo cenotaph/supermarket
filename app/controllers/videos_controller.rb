37022211adeeb28d858fce7b46559a5c90ffc79c
John W. Fail <john@pixelache.ac>
2019-09-23 11:39:43 +0300

    closes rollbar #34

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,10 +7,14 @@ class VideosController < ApplicationController
             else
               Year.where(reveal_decisions: true).order('year DESC').first
             end
-    @years = Year.all.order('year desc').to_a.delete_if { |x| x.videos.published.empty? }
-    @videos = []
-    # @years.each do |year|
-    @videos = Video.published.order('created_at DESC').where('year_id  = ?', @year.id).page(params[:page]).per(6)
-    # end
+    if @year.nil?
+      redirect_to '/'
+    else
+      @years = Year.all.order('year desc').to_a.delete_if { |x| x.videos.published.empty? }
+      @videos = []
+      # @years.each do |year|
+
+      @videos = Video.published.order('created_at DESC').where('year_id  = ?', @year.id).page(params[:page]).per(6)
+    end
   end
 end
