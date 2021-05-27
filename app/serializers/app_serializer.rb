cdf8d1f0e14933723aa4d865f7b6d90dc87743d4
John W. Fail <john@ptarmigan.fi>
2021-05-27 12:22:25 +0300

    app slug

--- app/serializers/app_serializer.rb
+++ app/serializers/app_serializer.rb
@@ -1,6 +1,9 @@
 class AppSerializer
   include JSONAPI::Serializer
   attributes :organisation_name, :booth_granted, :granted_result, :booth_type, :written_country, :exhibitor_full_address, :organisation_email, :contact_email
+  attribute :space_slug do |app|
+    app.space.slug
+  end
   attribute :logo do |app|
     app.space.logo? ? app.space.logo.url(:profile) : nil
   end
